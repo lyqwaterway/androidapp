59002b2fd82b87d8f0b3cc07f57ef48eeba69463
wzy5223 <wzy5223@126.com>
2018-12-18 13:34:16 +0800

    add env.sh

---
+++ env.sh
@@ -0,0 +1,7 @@
+export NODE_HOME=/opt/node
+export PATH=$NODE_HOME/bin:$PATH
+export ANDROID_HOME=$HOME/Android/Sdk
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+export PATH=$PATH:$ANDROID_HOME/emulator
